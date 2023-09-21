d172450cd626bc040d6ac059ddbb2b39d8d61021
shreyanzjain <shreyanz32@gmail.com>
2023-09-21 13:17:42 +0530

    add cookieParser

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,12 @@ var {create_user, login_user} = require('./methods/user_methods');
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 const express = require('express');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const port = 3000;
+
+app.use(cookieParser());
 
 app.post("/user/create", jsonParser, (req, res) => {
     create_user(req.body.email, req.body.role, req.body.password);
@@ -13,6 +16,7 @@ app.post("/user/create", jsonParser, (req, res) => {
 
 app.post("/user/login", jsonParser, (req, res) => {
     login_user(req.body.email, req.body.password);
+    res.send({"bye": "bye"});
 })
 
 app.listen(port, ()=>{
