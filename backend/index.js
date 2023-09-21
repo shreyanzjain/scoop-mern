d8a9e7ef9fd24bc0cdbd21fd0b80899615cc8b36
shreyanzjain <shreyanz32@gmail.com>
2023-09-21 18:44:59 +0530

    feat: login sets a cookie (jwt)

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
-var {create_user, login_user} = require('./methods/user_methods');
+var {create_user, login_user, get_data_for_jwt} = require('./methods/user_methods');
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const app = express();
 const port = 3000;
@@ -14,10 +16,22 @@ app.post("/user/create", jsonParser, (req, res) => {
     res.send({user: "created"});
 })
 
-app.post("/user/login", jsonParser, (req, res) => {
-    login_user(req.body.email, req.body.password);
-    res.send({"bye": "bye"});
-})
+app.post("/user/login", jsonParser, async(req, res) => {
+    const is_correct_password = await login_user(req.body.email, req.body.password);
+    if(is_correct_password){
+        const user_dict = await get_data_for_jwt(req.body.email);
+        const token = jwt.sign(user_dict, process.env.JWT_SECRET_KEY);
+        return res
+        .cookie("access_token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+        })
+        .status(200)
+        .json({message: "Logged In Successfully"});
+    }   else {
+        res.send(is_correct_password);
+    }
+});
 
 app.listen(port, ()=>{
     console.log(`App listening on http://localhost:${port}`);
--- backend/methods/user_methods.js
+++ backend/methods/user_methods.js
@@ -1,5 +1,6 @@
 const { PrismaClient } = require('@prisma/client');
 const bcrypt = require('bcrypt');
+
 const SALT_ROUNDS = 8;
 
 const prisma = new PrismaClient();
@@ -15,12 +16,54 @@ async function create_user(email, role, password) {
             hashed_password: hashed_password
         }
     })
-
-    console.log(hashed_password.length)
+    .then(async()=>{
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
 }
 
 async function login_user(email, password) {
-    // method to login the user, and return a cookie to the browser
+    const user = await prisma.user.findFirst({
+        where: {
+            email: email,
+        }
+    })
+    .then(async()=>{
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
+
+    const is_correct_password = await bcrypt.compare(password, user.hashed_password);
+    return is_correct_password;
 }
 
-module.exports = {create_user, login_user};
+async function get_data_for_jwt(email){
+    const user = await prisma.user.findFirst({
+        select: {
+            id: true,
+        },
+        where: {
+            email: email,
+        }
+    })
+    .then(async()=>{
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
+
+    const user_dict = {id: user.id, email: email};
+    return user_dict;
+}
+module.exports = {create_user, login_user, get_data_for_jwt};
