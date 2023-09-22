bac3ee65e7ff92c51210714c977fecffa601626e
nautiyalmohit <mohit8nautiyal@gmail.com>
2023-09-22 12:14:45 +0530

    added support tailwindcss

---
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,21 @@
+import "./Dashboard.css"
+export default function() {
+    return (
+        <div>
+            <h1 className="text-6xl font-bold">Student Dashboard</h1>
+            <div>
+                <section className="dashboard-wrapper">
+                    <div className="dashboard-container">
+                        <div>
+
+                        </div>
+                        <div>
+                            
+                        </div>
+                    </div>
+                </section>                
+            </div>
+        </div>
+
+    );
+}
