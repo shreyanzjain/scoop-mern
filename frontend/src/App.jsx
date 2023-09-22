8edc42e803a902f1564787730062ca7a568834bd
nautiyalmohit <mohit8nautiyal@gmail.com>
2023-09-22 12:50:19 +0530

    Added routing for landing and dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,37 @@
 import styled from "styled-components";
 import LandingPage from "./components/landing/LandingPage";
+import Home from "./components/Home";
+import Dashboard from "./components/Dashboard/Dashboard.jsx";
+import { Link, Routes, Route } from "react-router-dom";
+import './App.css'
+import "../index.css"
+
+
 function App() {
 
   return (
-    <LandingPage/>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to ="/">Home </Link>
+          </li>
+          <li>
+            <Link to ="/dashboard">Dashboard </Link>
+          </li>
+          <li>
+            <Link to ="/landing">Landing </Link>
+          </li>
+        </ul>
+      </nav>
+      
+      <Routes>
+        <Route path="/" element = {<Home />} />
+        <Route path="/dashboard" element = {<Dashboard />} />
+        <Route path="/landing" element = {<LandingPage />} />
+        
+      </Routes>
+    </>
   )
 }
 
