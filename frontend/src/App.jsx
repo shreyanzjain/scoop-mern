ebfc64a83d04c23edea0a6d7a329271a51d8903f
shreyanzjain <shreyanz32@gmail.com>
2023-09-22 12:57:38 +0530

    init2: placement dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,40 @@
 import styled from "styled-components";
 import LandingPage from "./components/landing/LandingPage";
+import Placement from "./components/Placement/Placement";
 import Home from "./components/Home";
 import Dashboard from "./components/Dashboard/Dashboard.jsx";
 import { Link, Routes, Route } from "react-router-dom";
-import './App.css'
-import "../index.css"
-
+import "./App.css";
+import "../index.css";
 
 function App() {
-
   return (
     <>
       <nav>
         <ul>
           <li>
-            <Link to ="/">Home </Link>
+            <Link to="/">Home </Link>
           </li>
           <li>
-            <Link to ="/dashboard">Dashboard </Link>
+            <Link to="/dashboard">Dashboard </Link>
           </li>
           <li>
-            <Link to ="/landing">Landing </Link>
+            <Link to="/landing">Landing </Link>
+          </li>
+          <li>
+            <Link to="/placement-cell"> Placement </Link>
           </li>
         </ul>
       </nav>
-      
+
       <Routes>
-        <Route path="/" element = {<Home />} />
-        <Route path="/dashboard" element = {<Dashboard />} />
-        <Route path="/landing" element = {<LandingPage />} />
-        
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/landing" element={<LandingPage />} />
+        <Route path="/placement-cell" element={<Placement />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
