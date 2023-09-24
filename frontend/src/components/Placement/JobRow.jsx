3c09d7a96b92738528d8ea71ac9c38d277f9d8ba
shreyanzjain <shreyanz32@gmail.com>
2023-09-24 19:12:56 +0530

    implement dynamic joblist in the placement dashboard using dummy data

---
+++ frontend/src/components/Placement/JobRow.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import colorStatus from "./ColorStatus.json";
+import OpenInNewRoundedIcon from '@mui/icons-material/OpenInNewRounded';
+
+function JobRow({title, company, status}) {
+  return (
+      <div className="container flex px-2 items-center justify-between bg-whitesmoke border-b-2 border-licorice h-1/6">
+        <div className="flex items-center">
+          <div className="ms-2">
+            <p className="text-2xl font-bold">{title}</p>
+            <p className="text-lg ">{company}</p>
+          </div>
+        </div>
+        <div className="flex items-center justify-between gap-2">
+          <div>
+            <a className="border-2 rounded-xl px-2 py-1 border-licorice" style={{backgroundColor: colorStatus[status]}}>
+              {status}
+            </a>
+          </div>
+          <div>
+            <a
+              href="#"
+              className="flex items-center justify-between border-2 rounded-xl gap-1 px-2 py-0.5 border-licorice bg-licorice text-whitesmoke"
+            >
+              Details
+              <OpenInNewRoundedIcon />
+            </a>
+          </div>
+        </div>
+      </div>
+  );
+}
+
+export default JobRow;
--- frontend/src/components/Placement/PlacementJobs.jsx
+++ frontend/src/components/Placement/PlacementJobs.jsx
@@ -1,28 +1,13 @@
 import React from "react";
 import WorkIcon from '@mui/icons-material/Work';
-import OpenInNewRoundedIcon from '@mui/icons-material/OpenInNewRounded';
+import JobRow from "./JobRow";
+import jobs from "./Data.json"
 
 function PlacementJobs() {
-  const jobs = {
-    job1: {
-      title: "SDE 1",
-      company: "Apple",
-      status: "Completed",
-      salary: "Rs. 4,00,000"
-    },
-    job2: {
-      title: "Options Trader",
-      company: "Uber",
-      status: "Recieving Applications",
-      salary: "Rs. 6,00,000"
-    },
-    job3: {
-      title: "Analyst",
-      company: "Kotak Bank",
-      status: "Interviewing",
-      salary: "Rs. 3,68,235"
-    },
-  };
+  const jobList = jobs.map(
+    ({title, company, status}) => 
+    (<JobRow title={title} company={company} status={status}/>)
+  );
   return (
     <div className="container pb-6 w-5/6 bg-whitesmoke bg-opacity-30" style={{height: "93vh"}}>
       <div className="flex text-2xl font-bold text-whitesmoke mt-2 ms-2 me-2 rounded-t-lg h-1/6 bg-licorice items-center justify-start">
@@ -30,55 +15,8 @@ function PlacementJobs() {
         <p className="ms-2">Jobs List</p>
       </div>
       <div className="flex-col text-lg mt-2 ms-2 me-2 h-5/6 bg-whitesmoke text-licorice">
-        {/* When there are jobs posted */}
-        <div className="container flex px-2 items-center justify-between bg-whitesmoke border-b-2 border-licorice h-1/6">
-          <div className="flex items-center">
-            <div className="ms-2">
-              <p className="text-2xl font-bold">{jobs.job1.title}</p>
-              <p className="text-lg ">{jobs.job1.company}</p>
-            </div>
-          </div>
-          <div className="flex items-center justify-between gap-2">
-            <div>
-              <a className="border-2 rounded-xl px-2 py-1 border-licorice bg-ashgray">{jobs.job1.status}</a>
-            </div> 
-            <div>
-              <a href="#" className="flex items-center justify-between border-2 rounded-xl gap-1 px-2 py-0.5 border-licorice bg-licorice text-whitesmoke">Details<OpenInNewRoundedIcon/></a>
-            </div>
-          </div>
-        </div>
-        <div className="container flex px-2 items-center justify-between bg-whitesmoke border-b-2 border-licorice h-1/6">
-          <div className="flex items-center">
-            <div className="ms-2">
-              <p className="text-2xl font-bold">{jobs.job2.title}</p>
-              <p className="text-lg ">{jobs.job2.company}</p>
-            </div>
-          </div>
-          <div className="flex items-center justify-between gap-2">
-            <div>
-              <a className="border-2 rounded-xl px-2 py-1 border-licorice bg-whitesmoke">{jobs.job2.status}</a>
-            </div> 
-            <div>
-              <a href="#" className="flex items-center justify-between border-2 rounded-xl gap-1 px-2 py-0.5 border-licorice bg-licorice text-whitesmoke">Details<OpenInNewRoundedIcon/></a>
-            </div>
-          </div>
-        </div>
-        <div className="container flex px-2 items-center justify-between bg-whitesmoke border-b-2 border-licorice h-1/6">
-          <div className="flex items-center">
-            <div className="ms-2">
-              <p className="text-2xl font-bold">{jobs.job3.title}</p>
-              <p className="text-lg ">{jobs.job3.company}</p>
-            </div>
-          </div>
-          <div className="flex items-center justify-between gap-2">
-            <div>
-              <a className="border-2 rounded-xl px-2 py-1 border-licorice bg-tan">{jobs.job3.status}</a>
-            </div> 
-            <div>
-              <a href="#" className="flex items-center justify-between border-2 rounded-xl gap-1 px-2 py-0.5 border-licorice bg-licorice text-whitesmoke">Details<OpenInNewRoundedIcon/></a>
-            </div>
-          </div>
-        </div>    
+        {jobList}
+        {/* When there are jobs posted */}    
         {/* When there are no jobs posted */}
         {/* <div className="px-4 pb-2 border-b-2 border-ebony container flex items-center justify-between">
           <div className="w-3/6"></div>
