990303d3b3e11b002d42dac8aeb8f156c87d6aa9
Nikunj Mistry <nikunjmistry1000@gmail.com>
2023-09-26 18:51:06 +0530

    Dynamic Dates JSON File for Calendar

--- frontend/src/components/Calendar/Calendar.jsx
+++ frontend/src/components/Calendar/Calendar.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
+import calendarData from './calendarData.json';
 
 function Calendar() {
   const currentYear = new Date().getFullYear();
   const [displayedYear, setDisplayedYear] = useState(currentYear);
+  const [selectedMonth, setSelectedMonth] = useState(null);
 
   const goToPrevYear = () => {
     if (displayedYear > currentYear - 5) {
@@ -14,6 +16,16 @@ function Calendar() {
     if (displayedYear < currentYear) {
       setDisplayedYear(displayedYear + 1);
     }
+  };
+
+  const handleMonthClick = (month) => {
+    // Filter the calendarData to get activities for the selected month
+    const activitiesForMonth = calendarData.filter(
+      (entry) => entry.year === displayedYear && entry.month === month
+    );
+
+    // Display the activities for the selected month
+    setSelectedMonth(activitiesForMonth);
   };
 
   const renderCalendar = () => {
@@ -38,7 +50,11 @@ function Calendar() {
         </div>
         <div className="grid grid-cols-4 gap-4">
           {Array.from({ length: 12 }, (_, index) => (
-            <div key={index} className="w-12 h-12 border rounded-full flex items-center justify-center bg-white hover:bg-yellow-300 transition duration-300 ease-in-out transform hover:scale-110">
+            <div
+              key={index}
+              className="w-12 h-12 border rounded-full flex items-center justify-center bg-white hover:bg-yellow-300 transition duration-300 ease-in-out transform hover:scale-110 cursor-pointer"
+              onClick={() => handleMonthClick(index + 1)}
+            >
               {index + 1}
             </div>
           ))}
@@ -50,6 +66,20 @@ function Calendar() {
   return (
     <div className="flex flex-col items-center mt-8">
       {renderCalendar()}
+      <div className="mt-4">
+        {selectedMonth ? (
+          <div>
+            <h2>Selected Month Info</h2>
+            {selectedMonth.map((activity, index) => (
+              <p key={index}>{`${activity.activity} on ${activity.month}/${activity.day}/${activity.year}`}</p>
+            ))}
+          </div>
+        ) : (
+          <div>
+            <h2>Select a Month</h2>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
