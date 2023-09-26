fb1c90e7d3f917558beb17cef7a8a23a802fd2ea
Nikunj Mistry <nikunjmistry1000@gmail.com>
2023-09-26 18:28:56 +0530

    Started Calendar Component

--- frontend/src/components/Calendar/Calendar.jsx
+++ frontend/src/components/Calendar/Calendar.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const Calendar = () => {
+function Calendar() {
+  const currentYear = new Date().getFullYear();
+  const [displayedYear, setDisplayedYear] = useState(currentYear);
+
+  const goToPrevYear = () => {
+    if (displayedYear > currentYear - 5) {
+      setDisplayedYear(displayedYear - 1);
+    }
+  };
+
+  const goToNextYear = () => {
+    if (displayedYear < currentYear) {
+      setDisplayedYear(displayedYear + 1);
+    }
+  };
+
+  const renderCalendar = () => {
+    return (
+      <div className="p-4 border rounded-lg text-center bg-gradient-to-b from-purple-300 via-purple-300 to-purple-300  absolute top-8 right-4">
+        <div className="flex justify-between mb-4">
+          <button
+            onClick={goToPrevYear}
+            className={`w-12 h-12 rounded-full border bg-blue-500 hover:bg-indigo-600 text-white text-2xl`}
+            disabled={displayedYear <= currentYear - 5}
+          >
+            &lt;
+          </button>
+          <h2 className="text-xl font-bold text-white">{displayedYear}</h2>
+          <button
+            onClick={goToNextYear}
+            className={`w-12 h-12 rounded-full border bg-blue-500 hover:bg-green-600 text-white text-2xl`}
+            disabled={displayedYear >= currentYear}
+          >
+            &gt;
+          </button>
+        </div>
+        <div className="grid grid-cols-4 gap-4">
+          {Array.from({ length: 12 }, (_, index) => (
+            <div key={index} className="w-12 h-12 border rounded-full flex items-center justify-center bg-white hover:bg-yellow-300 transition duration-300 ease-in-out transform hover:scale-110">
+              {index + 1}
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  };
+
   return (
-    <div>Calendar</div>
-  )
+    <div className="flex flex-col items-center mt-8">
+      {renderCalendar()}
+    </div>
+  );
 }
 
-export default Calendar
+export default Calendar;
--- frontend/src/components/ResetPassword/ResetPwd.jsx
+++ frontend/src/components/ResetPassword/ResetPwd.jsx
@@ -75,10 +75,8 @@ const ResetPwd = () => {
     <div className="z-0 bg-white py-6 sm:py-8 lg:py-12">
       <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
         <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-8 lg:text-3xl">Reset Password</h2>
-
         <form className="mx-auto max-w-lg rounded-lg border">
           <div className="flex flex-col gap-4 p-4 md:p-8">
-            
             <div>
               <label htmlFor="email" className="mb-2 inline-block text-sm text-gray-800 sm:text-base">Email</label>
               <input
