be3437faa1242839fb6b03ecb1f1eb9ea59508f1
shreyanzjain <shreyanz32@gmail.com>
2023-09-28 22:08:35 +0530

    Implemented the studentlist using placeholder data

--- frontend/src/components/Placement/PlacementStudents.jsx
+++ frontend/src/components/Placement/PlacementStudents.jsx
@@ -1,25 +1,25 @@
 import React from "react";
-import WorkIcon from "@mui/icons-material/Work";
-import JobRow from "./JobRow";
-import jobs from "./Data.json";
+import WorkIcon from '@mui/icons-material/Work';
+import StudentRow from "./StudentRow";
+import students from "./Students.json"
 
-function PlacementJobs() {
-  const jobList = jobs.map(({ title, company, status }) => (
-    <JobRow title={title} company={company} status={status} />
-  ));
+function PlacementStudents() {
+  const studentList = students.map(
+    ({f_name, l_name, branch, verified}) => 
+    (<StudentRow f_name={f_name} l_name={l_name} branch={branch} verification_status={verified}/>)
+  );
   return (
     <div className="container m-2 w-5/6 bg-whitesmoke bg-opacity-30">
       <div className="flex text-2xl font-bold text-whitesmoke rounded-t-lg h-1/6 bg-licorice items-center justify-start">
-        <WorkIcon className="ms-4" />
-        <p className="ms-2">Jobs List</p>
+        <WorkIcon className="ms-4"/>
+        <p className="ms-2">Students List</p>
       </div>
       <div className="flex-col h-5/6 text-lg mt-2 bg-whitesmoke text-licorice">
-        {jobList}
-        {/* When there are jobs posted */}
-        {/* When there are no jobs posted */}
+        {studentList}
+        {/* When there are students added */}    
         {/* <div className="px-4 pb-2 border-b-2 border-ebony container flex items-center justify-between">
           <div className="w-3/6"></div>
-          <p className="me-2">Nothing to see here. Have you added any jobs yet?</p>
+          <p className="me-2">Nothing to see here. Have you added any students yet?</p>
           <button className="border-2 rounded-lg px-2 border-licorice text-licorice hover:bg-licorice hover:text-whitesmoke">Add Jobs</button>
         </div> */}
       </div>
@@ -27,4 +27,4 @@ function PlacementJobs() {
   );
 }
 
-export default PlacementJobs;
+export default PlacementStudents;
