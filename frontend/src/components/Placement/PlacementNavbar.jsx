25b99ac618aa3f5d3dd694e8a302d22a8013d50e
shreyanzjain <shreyanz32@gmail.com>
2023-09-28 16:06:09 +0530

    enable vite-mkcert for ssl on the frontend

--- frontend/src/components/Placement/PlacementNavbar.jsx
+++ frontend/src/components/Placement/PlacementNavbar.jsx
@@ -1,6 +1,34 @@
 import React from "react";
 import vTube from "../img/half_logo.png";
-import AttributionIcon from '@mui/icons-material/Attribution';
+import AttributionIcon from "@mui/icons-material/Attribution";
+import axios from "axios";
+
+function login() {
+  axios.post("https://127.0.0.1:3000/user/login", {
+    email: "varun@nikunj.com",
+    password: "varunnikunj",
+  }, {
+    withCredentials: true
+  })
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+}
+
+function protected_check() {
+  axios.get("https://127.0.0.1:3000/protected", {
+    withCredentials: true
+  })
+  .then((response)=> {
+    console.log(response);
+  })
+  .catch((error)=> {
+    console.log(error);
+  })
+}
 
 function PlacementNavbar() {
   const user = "Shreyans";
@@ -9,11 +37,15 @@ function PlacementNavbar() {
       <div className="max-w-full mx-auto">
         <div className="flex items-center justify-between h-12">
           <span className="flex ms-2 text-2xl text-licorice font-semibold items-center">
-            <AttributionIcon className="text-licorice me-2"/> {user}
+            <AttributionIcon className="text-licorice me-2" /> {user}
           </span>
           <div className="flex space-x-4 text-licorice me-2">
-            <a className="hover:bg-ashgray px-2" href="#">Report An Issue</a>
-            <a className="hover:bg-ashgray px-2" href="#">Log Out</a>
+            <a className="hover:bg-ashgray px-2" href="#" onClick={protected_check}>
+              Report An Issue
+            </a>
+            <a className="hover:bg-ashgray px-2" href="#" onClick={login}>
+              Log Out
+            </a>
           </div>
         </div>
       </div>
