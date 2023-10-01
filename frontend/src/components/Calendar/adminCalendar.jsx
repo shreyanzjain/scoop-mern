cd252ad7337fd4f549aeaccf0c3e17d7f9032f50
Nikunj Mistry <nikunjmistry1000@gmail.com>
2023-10-01 17:04:01 +0530

    Proper Styling of Admin Calendar

--- frontend/src/components/Calendar/adminCalendar.jsx
+++ frontend/src/components/Calendar/adminCalendar.jsx
@@ -53,51 +53,58 @@ function AdminCalendar() {
   };
 
   return (
-    <div className="flex justify-center items-center min-h-screen">
+    <div className="flex justify-center items-center min-h-screen absolute top-4 w-full">
       <div className="text-center">
         <h1 className="text-3xl font-semibold mb-4">Admin Calendar</h1>
-        <Calendar onChange={handleDateChange} value={date} onClickDay={handleDateClick} />
-        {selectedDate && (
-          <div className="mt-4">
-            <p className="text-lg mb-2">Selected Date: {selectedDate.toDateString()}</p>
-            <input
-              type="text"
-              placeholder="Enter event title"
-              className="border p-2 w-64 mx-auto rounded-md"
-              value={eventTitle}
-              onChange={handleEventTitleChange}
-            />
-            <input
-              type="number"
-              placeholder="Enter event duration (in days)"
-              className="border p-2 w-64 mx-auto rounded-md mt-2"
-              value={eventDuration}
-              onChange={handleEventDurationChange}
-            />
-            <select
-              className="border p-2 w-64 mx-auto rounded-md mt-2"
-              value={category}
-              onChange={handleCategoryChange}
-            >
-              <option value="College">College</option>
-              <option value="Placement">Placement</option>
-            </select>
-            <button
-              onClick={scheduleEvent}
-              className="mt-2 bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-md"
-            >
-              Schedule Event
-            </button>
-          </div>
-        )}
+        
+        {/* Calendar */}
+        <div className="mb-4 mx-auto w-72">
+          <Calendar onChange={handleDateChange} value={date} onClickDay={handleDateClick} />
+          {selectedDate && (
+            <p className="text-lg mt-4">Selected Date: {selectedDate.toDateString()}</p>
+          )}
+        </div>
+        
+        {/* Event Scheduling Section */}
+        <div className="mt-4">
+          <input
+            type="text"
+            placeholder="Enter event title"
+            className="border p-2 w-64 mx-auto rounded-md"
+            value={eventTitle}
+            onChange={handleEventTitleChange}
+          />
+          <input
+            type="number"
+            placeholder="Enter event duration (in days)"
+            className="border p-2 w-64 mx-auto rounded-md mt-2"
+            value={eventDuration}
+            onChange={handleEventDurationChange}
+          />
+          <select
+            className="border p-2 w-64 mx-auto rounded-md mt-2"
+            value={category}
+            onChange={handleCategoryChange}
+          >
+            <option value="College">College</option>
+            <option value="Placement">Placement</option>
+          </select>
+          <button
+            onClick={scheduleEvent}
+            className="mt-2 bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-md"
+          >
+            Schedule Event
+          </button>
+        </div>
+        
         <div className="mt-6">
           <h2 className="text-2xl font-semibold">Scheduled Events</h2>
           <ul className="list-disc mt-2">
             {events.map((event, index) => (
               <li key={index} className="text-lg">
-                {`Start Date: ${event.startDate.toDateString()}, Event Title: "start of ${event.title}", Category: ${event.category}`}<p></p>
-
-                {`End Date: ${event.endDate.toDateString()}, Event Title: "end of ${event.title}", Category: ${event.category}`}
+                {`Start Date: ${event.startDate.toDateString()}, Event Title: "${event.title}", Category: ${event.category}`}
+                <p></p>
+                {`End Date: ${event.endDate.toDateString()}, Event Title: "${event.title}", Category: ${event.category}`}
               </li>
             ))}
           </ul>
