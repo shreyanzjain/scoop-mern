7d33e4d12b7aaed94eb7d1a06c228c8abe8113aa
shreyanzjain <shreyanz32@gmail.com>
2023-10-03 10:42:23 +0530

    frontend changes

--- frontend/src/components/Placement/PlacementJobs.jsx
+++ frontend/src/components/Placement/PlacementJobs.jsx
@@ -7,13 +7,16 @@ function PlacementJobs() {
   const jobList = jobs.map(({ title, company, status }) => (
     <JobRow title={title} company={company} status={status} />
   ));
+
+  
+
   return (
     <div className="container m-2 w-5/6 bg-whitesmoke bg-opacity-30">
       <div className="flex text-2xl font-bold text-whitesmoke rounded-t-lg h-1/6 bg-licorice items-center justify-start">
         <WorkIcon className="ms-4" />
         <p className="ms-2">Jobs List</p>
       </div>
-      <div className="flex-col h-5/6 text-lg mt-2 bg-whitesmoke text-licorice">
+      <div className="flex-col h-5/6 text-lg mt-2 bg-whitesmoke text-licorice overflow-y-scroll">
         {jobList}
         {/* When there are jobs posted */}
         {/* When there are no jobs posted */}
