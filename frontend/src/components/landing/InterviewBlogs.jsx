1678d0284d952c10b8274f6f50290744da627286
Nikunj Mistry <nikunjmistry1000@gmail.com>
2023-10-20 15:25:15 +0530

    Blogs Display

--- frontend/src/components/landing/InterviewBlogs.jsx
+++ frontend/src/components/landing/InterviewBlogs.jsx
@@ -1,51 +1,58 @@
 import React, { useState } from 'react';
 
-// Sample company data with multiple users
 const companyData = {
     "Company A": [
         {
             user: "Anonymous User 1",
             batchYear: "2022",
-            role: "Role 1"
+            role: "Role 1",
+            information: "Anonymous User 1 is a talented individual who joined Company A in 2022. They play a crucial role in the company, serving as Role 1. With their dedication and skills, they contribute significantly to the success of the team. Anonymous User 1 is known for their excellent problem-solving abilities and teamwork, making them an invaluable asset to the company."
         },
+        
         {
             user: "Anonymous User 2",
             batchYear: "2021",
-            role: "Role 2"
+            role: "Role 2",
+            information: "Additional information for User 2."
         },
         {
             user: "Anonymous User 3",
             batchYear: "2023",
-            role: "Role 3"
+            role: "Role 3",
+            information: "Additional information for User 3."
         }
     ],
     "Company B": [
         {
             user: "User X",
             batchYear: "2020",
-            role: "Role X"
+            role: "Role X",
+            information: "Additional information for User X."
         },
         {
             user: "User Y",
             batchYear: "2019",
-            role: "Role Y"
+            role: "Role Y",
+            information: "Additional information for User Y."
         },
         {
             user: "User Z",
             batchYear: "2021",
-            role: "Role Z"
+            role: "Role Z",
+            information: "Additional information for User Z."
         }
     ]
     // Add data for other companies as needed
 };
 
 function InterviewBlogs() {
-    // Sample company names
     const companyNames = Object.keys(companyData);
-
-    // State to track the selected company and user
     const [selectedCompany, setSelectedCompany] = useState(null);
-    const [selectedUser, setSelectedUser] = useState(null);
+    const [selectedUserIndex, setSelectedUserIndex] = useState(null);
+
+    const selectUser = (index) => {
+        setSelectedUserIndex(index);
+    };
 
     return (
         <div className="p-2 bg-gray-100 border rounded shadow-md">
@@ -60,7 +67,7 @@ function InterviewBlogs() {
                         className="p-2 pr-2 border rounded"
                         onChange={(e) => {
                             setSelectedCompany(e.target.value);
-                            setSelectedUser(null);
+                            setSelectedUserIndex(null);
                         }}
                     >
                         <option value="">Select a company</option>
@@ -71,23 +78,42 @@ function InterviewBlogs() {
                         ))}
                     </select>
                 </div>
-                {selectedCompany && (
-                    <div className="absolute top-16 left-0 right-0 bg-white border border-gray-300 p-4">
-                        {companyData[selectedCompany].map((user, index) => (
-                            <div
-                                key={index}
-                                className={`mb-4 p-2 border border-gray-400 cursor-pointer ${
-                                    selectedUser === index && 'bg-blue-100'
-                                }`}
-                                onClick={() => setSelectedUser(index)}
-                            >
-                                <div className="flex justify-between">
-                                    <div className="text-xl font-bold">{user.user}</div>
-                                    <div className="text-right">Batch Year: {user.batchYear}</div>
+                {selectedCompany ? (
+                    <div className="absolute top-16 left-0 right-0 bg-white border-gray-300 p-4">
+                        {selectedUserIndex === null ? (
+                            companyData[selectedCompany].map((user, index) => (
+                                <div
+                                    key={index}
+                                    className="mb-4 p-2 border border-gray-400 cursor-pointer"
+                                    onClick={() => selectUser(index)}
+                                >
+                                    <div className="flex justify-between">
+                                        <div className="text-xl font-bold">{user.user}</div>
+                                        <div className="text-right">Batch Year: {user.batchYear}</div>
+                                    </div>
+                                    <div className="text-left text-bottom">Role: {user.role}</div>
                                 </div>
-                                <div className="text-left text-bottom">Role: {user.role}</div>
+                            ))
+                        ) : (
+                            <div className="mb-4 p-2 border border-gray-400 relative">
+                                <div className="text-xl font-bold text-center mb-4">
+                                {companyData[selectedCompany][selectedUserIndex].user}, {companyData[selectedCompany][selectedUserIndex].batchYear} - {companyData[selectedCompany][selectedUserIndex].role}
+                                </div>
+                                <div className="text-justify" style={{ padding: '0 15%' }}>{companyData[selectedCompany][selectedUserIndex].information}</div>
+                                <div className="text-left text-bottom">
+                                    <button
+                                        className="text-blue-500 cursor-pointer absolute top-0 right-0 p-2"
+                                        onClick={() => selectUser(null)}
+                                    >
+                                        Close
+                                    </button>
+                                </div>
                             </div>
-                        ))}
+                        )}
+                    </div>
+                ) : (
+                    <div className="text-center text-3xl font-bold text-gray-500">
+                        Select the company you want
                     </div>
                 )}
             </div>
