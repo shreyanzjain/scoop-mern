76f6bb920ea0109a92b69a9eafeecc63ad0a0e36
shreyanzjain <shreyanz32@gmail.com>
2023-10-29 15:20:34 +0530

    remove placement routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,23 +2,22 @@ import styled from "styled-components";
 import { Link, Routes, Route } from "react-router-dom";
 
 import LandingPage from "./components/landing/LandingPage";
-import Why_tsec  from "./components/landing/Why_tsec";
+import Why_tsec from "./components/landing/Why_tsec";
 import Signup_Login from "./components/landing/Signup_Login";
 import Placement from "./components/Placement/Placement";
 
 import Home from "./components/landing/Home";
 import Dashboard from "./components/Dashboard/Dashboard.jsx";
 
-
 import UserUI from "./components/UserUI/UserUI";
 import Calendar from "./components/Calendar/Calendar";
 import AdminCalendar from "./components/Calendar/adminCalendar";
-import UploadJob from "./components/Placement/JobUp";
+
 import LearnMore from "./components/landing/LearnMore";
 import ResetPwd from "./components/ResetPassword/ResetPwd";
 import "./App.css";
 import "../index.css";
-import JobUp from "./components/Placement/JobUp";
+
 import PlacementJobs from "./components/Placement/PlacementJobs";
 import PlacementDashboard from "./components/Placement/PlacementDashboard";
 import PlacementStudents from "./components/Placement/PlacementStudents";
@@ -45,7 +44,7 @@ const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<LandingPage />} >
+        <Route path="/" element={<LandingPage />}>
           <Route index element={<Home />} />
           <Route path="home" element={<Home />} />
           <Route path="whytsec" element={<Why_tsec />} />
@@ -56,28 +55,20 @@ const App = () => {
           <Route path="ContactUs" element={<ContactUs />} />
           <Route path="OurRecruiters" element={<OurRecruiters />} />
           <Route path="CollegeEnvoys" element={<CollegeEnvoys />} />
-          <Route path="InterviewBlogs" element={<InterviewBlogs/>} />
-          <Route path="Alumnie" element={<Alumnie/>} />
-        </ Route>
-        
-        <Route path="placement-cell" element={<Placement />}>
-          <Route index element={<PlacementDashboard/>}></Route>
-          <Route path="jobs" element={<PlacementJobs />}></Route>
-          <Route path="dashboard" element={<PlacementDashboard />}></Route>
-          <Route path="students" element={<PlacementStudents/>}></Route>
-          <Route path="upload" element={<PlacementUpload/>}></Route>
+          <Route path="InterviewBlogs" element={<InterviewBlogs />} />
+          <Route path="Alumnie" element={<Alumnie />} />
         </Route>
 
+        <Route path="placement-cell" element={<Placement />}></Route>
+
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="user-page" element={<UserUI />} >
+        <Route path="user-page" element={<UserUI />}>
           <Route index element={<Dashboard />} />
           <Route path="user-profile" element={<UserProfile />} />
           <Route path="dashboard" element={<Dashboard />} />
         </Route>
         <Route path="/calendar" element={<Calendar />} />
         <Route path="/adminCalendar" element={<AdminCalendar />} />
-        
-        <Route path="/uploadJob" element={<JobUp />} />
       </Routes>
     </>
   );
