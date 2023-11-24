8d3dcbba1b1718a690b7de7053606c3b9e592efa
shreyanzjain <shreyanz32@gmail.com>
2023-11-24 23:58:05 +0530

    refactor: single prisma instance across codebase

--- backend/instance/init.js
+++ backend/instance/init.js
@@ -1,6 +1,16 @@
 // Defines our prisma instance that will be used throughout the backend.
 
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+
+process.on("SIGINT", async () => {
+  await prisma.$disconnect();
+  process.exit(1);
+});
+
+process.on("SIGTERM", async () => {
+  await prisma.$disconnect();
+  process.exit(1);
+});
 
 module.exports = prisma;
