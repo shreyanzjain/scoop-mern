7d24be00cda9397b6d231f303c4899e6209593d5
Varun Iyer <iyervarunnurav@gmail.com>
2024-01-18 18:43:43 +0530

    Refactored the code of App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,11 @@
-import styled from "styled-components";
 import { Link, Routes, Route } from "react-router-dom";
 
 import LandingPage from "./components/landing/LandingPage";
 import Placement from "./components/Placement/Placement";
-
-
-import Dashboard from "./components/Dashboard/Dashboard.jsx";
 import UserUI from "./components/UserUI/UserUI";
-import Calendar from "./components/Calendar/Calendar";
-import AdminCalendar from "./components/Calendar/adminCalendar";
-
 
 import "./App.css";
 import "../index.css";
-
-import UserProfile from "./components/UserUI/UserProfile";
-
-const Container = styled.div`
-  display: flex;
-`;
-
-const Main = styled.div`
-  flex: 7;
-`;
-const Wrapper = styled.div``;
 
 const App = () => {
   return (
@@ -35,14 +17,11 @@ const App = () => {
 
         <Route path="placement-cell" element={<Placement />}></Route>
 
-        <Route path="/dashboard" element={<Dashboard />} />
+        
         <Route path="user-page" element={<UserUI />}>
-          <Route index element={<Dashboard />} />
-          <Route path="user-profile" element={<UserProfile />} />
-          <Route path="dashboard" element={<Dashboard />} />
+          
         </Route>
-        <Route path="/calendar" element={<Calendar />} />
-        <Route path="/adminCalendar" element={<AdminCalendar />} />
+        
       </Routes>
     </>
   );
