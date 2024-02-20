2bc2bde9291c2d89b48d3db68f286edb29ece2a1
Varun Iyer <iyervarunnurav@gmail.com>
2024-02-20 22:29:44 +0530

    unnecesary code removed

--- frontend/src/components/landing/LearnMore.jsx
+++ frontend/src/components/landing/LearnMore.jsx
@@ -71,9 +71,7 @@ const LearnMore = () => {
                 </p>
               </div>
             </div>
-            <button class="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-              <NavLink to="/">Home</NavLink>
-            </button>
+            
           </div>
         </section>
       </div>
